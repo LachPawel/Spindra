85f1fc03e6fad261fe227681c41d08cad1b00061
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-10-11 10:40:42 +0200

    Initial Commit

---
+++ Spindra/SpindraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpindraApp.swift
+//  Spindra
+//
+//  Created by Pawel Kowalewski on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpindraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
